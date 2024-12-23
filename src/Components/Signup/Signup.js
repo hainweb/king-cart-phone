1458caff367d56eed0fa8c280fdda876128fb3b7
hainweb <hainofficialweb@gmail.com>
2024-12-23 18:34:36 +0530

    Update Signup.js

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'axios'; 
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../Urls/Urls';
